2f7b229e92539250660e0b877dc0308b46de0326
Balázs Barta <info@hellobala.hu>
2024-11-27 14:17:05 +0100

    Add structure to Studio

--- sanity.config.ts
+++ sanity.config.ts
@@ -4,15 +4,49 @@ import {visionTool} from '@sanity/vision'
 import {codeInput} from '@sanity/code-input'
 import {colorInput} from '@sanity/color-input'
 import {schemaTypes} from './schemaTypes'
+import {BookIcon, DocumentsIcon, TagIcon, UserIcon, BulbOutlineIcon, ComponentIcon} from '@sanity/icons'
 
 export default defineConfig({
   name: 'default',
-  title: 'mp-headless',
+  title: 'Machinery Partner CMS',
 
   projectId: 'yhrhi1m6',
   dataset: 'production',
 
-  plugins: [structureTool(), visionTool(), codeInput(), colorInput()],
+  plugins: [
+    structureTool({
+      structure: (S) =>
+        S.list()
+          .title('Content')
+          .items([
+            // Blog folder
+            S.listItem()
+              .title('Blog')
+              .icon(BookIcon)
+              .child(
+                S.list()
+                  .title('Blog')
+                  .items([
+                    S.listItem()
+                      .title('Posts')
+                      .icon(DocumentsIcon)
+                      .child(
+                        S.documentTypeList('blogPost')
+                      ),
+                    S.listItem()
+                      .title('Categories')
+                      .icon(TagIcon)
+                      .child(
+                        S.documentTypeList('blogCategory')
+                      ),
+                  ])
+              ),
+          ])
+    }),
+    visionTool(),
+    codeInput(),
+    colorInput()
+  ],
 
   schema: {
     types: schemaTypes,
--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -1,7 +1,6 @@
-import { postType } from './postType'
 import blogPost from './blogPost'
 import blogCategory from './blogCategory'
 import {youTubeType} from './youTubeType'
 import {bannerType} from './bannerType'
 
-export const schemaTypes = [postType, blogPost, blogCategory, youTubeType, bannerType]
+export const schemaTypes = [blogPost, blogCategory, youTubeType, bannerType]
