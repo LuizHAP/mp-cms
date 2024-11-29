e224cc07b383bfd474056db3b6753c5f82a28ec2
Balázs Barta <info@hellobala.hu>
2024-11-29 12:48:43 +0100

    Remove 'Heading 1' style from schemas.

--- schemaTypes/blogPost.ts
+++ schemaTypes/blogPost.ts
@@ -72,7 +72,6 @@ export default defineType({
           type: 'block',
           styles: [
             { title: 'Normal', value: 'normal' },
-            { title: 'Heading 1', value: 'h1' },
             { title: 'Heading 2', value: 'h2' },
             { title: 'Heading 3', value: 'h3' },
             { title: 'Heading 4', value: 'h4' },
